2b13ceb7fea99383dc8d4fc5d108ba4f73b83ba8
Solo one one <fally59100@gmail.com>
2021-03-09 11:31:13 +0000

    methode replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ###Tokenization du vocabulaire :
 
-vocabulary = open("dictionnaire.txt","r",encoding = "utf-8").read()
+vocabulary = open("dictionnaire.txt","r",encoding="utf-8").read()
 print(vocabulary)
 
 ### on va creer une liste de mots:
@@ -10,4 +10,9 @@ print(tokenization_vocabulary)
 ###remplacement des caracteres speciaux :
 f = open("texte.txt","r",encoding="utf-8")
 text_string = f.read()
+print(text_string)
+
+##method replace j'enleve la ponctuation 
+text_string = text_string.replace(","," ")
+text_string = text_string.replace("."," ")
 print(text_string)
