ed1d4637d1bd36f4f6449aadb22885026fda59e9
Kassama Fally <fally59100@gmail.com>
2021-03-09 11:45:59 +0000

    name of exercice

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 ###Tokenization du vocabulaire :
 
 vocabulary = open("dictionnaire.txt","r",encoding="utf-8").read()
-print(vocabulary)
+
 
 ### on va creer une liste de mots:
 tokenization_vocabulary = vocabulary.split(" ")
-print(tokenization_vocabulary)
+
 
 ###remplacement des caracteres speciaux :
 f = open("texte.txt","r",encoding="utf-8")
 text_string = f.read()
-print(text_string)
+
 
 ##method replace j'enleve la ponctuation 
-text_string = text_string.replace(","," ")
-text_string = text_string.replace("."," ")
+text_string = text_string.replace(",","")
+text_string = text_string.replace(".","")
+text_string = text_string.replace("'","")
+text_string = text_string.replace("Ã©","e")
+text_string = text_string.replace("Ã","á")
+text_string = text_string.replace("\n","")
+
 print(text_string)
